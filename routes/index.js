2adda5ff245ab1c27254756c10d576ba024fd132
tarwat-uddin <t.uddin9121@gmail.com>
2022-01-24 11:11:57 +0000

    Add form view and GET/POST request handling for new messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var messages = [
+  {
+    text: "Hi there!",
+    user: "Elias",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Alexander",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  var { authorName, messageText } = req.body;
+  var message = {
+    text: messageText,
+    user: authorName,
+    added: new Date(),
+  };
+  messages.push(message);
+  res.redirect("/");
 });
 
 module.exports = router;
